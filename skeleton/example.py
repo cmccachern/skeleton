379586a1ba0ac89d34748c790ef557c6ef7ee911
cmccachern <carey@mccachern.com>
2019-09-08 19:26:29 -0600

    Run tests and add example

---
+++ skeleton/example.py
@@ -0,0 +1,30 @@
+"""
+This is an example module.  Every module should have a descriptive docsting.
+"""
+
+import numpy as np
+
+def sum_list(data):
+    """
+    Sum a list of numbers.
+
+    Each function should have a short description optionally followed, optionally by
+    a longer description.  Docstrings should follow the numpydoc format.  For more
+    information, see the numpy docstring guide at https://numpydoc.readthedocs.io/en/latest/format.html.
+
+
+    Parameters
+    ----------
+    data : list of int or float
+        Numbers to be summed.
+
+    Returns
+    -------
+    sum : float
+        Sum of input list
+    """
+    return np.sum(data)
+
+
+
+
---
+++ test/test_example.py
@@ -0,0 +1,21 @@
+"""
+Example unit test.  For more information on testing in python, see https://realpython.com/python-testing/.
+Run tests from the basic directory with 'python -m unittest discover'.  Note that you can also run
+unitests directly from PyCharm.
+"""
+
+import unittest
+
+from skeleton import example
+
+class TestExample(unittest.TestCase):
+    def test_list_int(self):
+        """
+        Test that it can sum a list of integers.
+        """
+        data = [1, 2, 3]
+        result = sum(data)
+        self.assertEqual(result, 6)
+
+if __name__ == '__main__':
+    unittest.main()
