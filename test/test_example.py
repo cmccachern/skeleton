55d093beb0ef8fca6aba28a48425672901eae0ec
cmccachern <carey@mccachern.com>
2019-09-08 20:17:16 -0600

    Fix pylint issues with test

--- test/test_example.py
+++ test/test_example.py
@@ -1,5 +1,6 @@
 """
-Example unit test.  For more information on testing in python, see https://realpython.com/python-testing/.
+Example unit test.  For more information on testing in python, see
+https://realpython.com/python-testing/.
 Run tests from the basic directory with 'python -m unittest discover'.  Note that you can also run
 unitests directly from PyCharm.
 """
@@ -9,12 +10,15 @@ import unittest
 from skeleton import example
 
 class TestExample(unittest.TestCase):
+    """
+    Test sum_list()
+    """
     def test_list_int(self):
         """
         Test that it can sum a list of integers.
         """
         data = [1, 2, 3]
-        result = sum(data)
+        result = example.sum_list(data)
         self.assertEqual(result, 6)
 
 if __name__ == '__main__':
